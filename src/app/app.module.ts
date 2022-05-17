a010a52cecefd86e034c8896cefcb3bf001dc168
Lenovo <w>
2022-05-17 01:10:48 -0500

    componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
 import { BarraComponent } from './components/barra/barra.component';
 import { NgChartsModule } from 'ng2-charts';
+import { PruebaComponent } from './prueba/prueba.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentsComponent,
-    BarraComponent
+    BarraComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule,
